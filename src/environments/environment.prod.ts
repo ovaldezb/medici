d06b6a87e7c55c6ef5a80a38b99e84c589039846
ovaldezb <omar.valdez.becerril@gmail.com>
2023-05-22 17:23:49 -0600

    add cognito data in prd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  cognito:{
+    userPoolId: 'us-east-2_GKiwU3lWX',
+    userPoolWebClientId:'57l9dr238motfdps6pidtu4v4k'
+  }
 };
